4431751f3d2a97268c34d4581bf1b789ab4c5c72
em-p <gremleaf@gmail.com>
2016-01-20 13:12:40 -0500

    adfsdf
    adfasdfads

---
+++ provscripts/db/contacts.sql
@@ -0,0 +1,6 @@
+CREATE TABLE contacts(ID INTEGER PRIMARY KEY, firstName TEXT, lastName TEXT,phoneNumber INTEGER);
+
+INSERT INTO table
+(firstName, lastName, phoneNumber)
+VALUES
+("test", "contact" , 6466968239 );
